9ed1f7ae2244da77da461c677035ce0816a4793f
Srinivas <srinivasmp.sri@gmail.com>
2021-03-14 20:59:47 +0100

    updated header with styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
-import HomePage from './pages/homepage/hompage.component'
-import ShopPage from './pages/shop/shop.component'
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/hompage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       {/* <HomePage></HomePage> */}
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
